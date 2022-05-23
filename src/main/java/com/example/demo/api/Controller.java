a5617f8b98122f816dd0d17aac0e1eb3aaa36baa
isabelPatulski <isab985d@stud.kea.dk>
2022-05-23 12:16:02 +0200

    Endpoint lavet

--- src/main/java/com/example/demo/api/Controller.java
+++ src/main/java/com/example/demo/api/Controller.java
@@ -1,4 +1,25 @@
 package com.example.demo.api;
 
+
+import com.example.demo.entity.Animal;
+import com.example.demo.repositories.AnimalRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/animals")
 public class Controller {
+    public Controller(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+    }
+
+    AnimalRepository animalRepository;
+
+    @GetMapping
+    public List<Animal> getAnimals() {
+        return animalRepository.findAll();
+    }
 }
